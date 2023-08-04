414a1f7d000cd7fa7105fd7f3855f65397992a03
Anton Istomin <istomanton@gmail.com>
2023-08-04 21:06:54 +0400

    Style layout of main page

--- src/components/main-page/MainPage.tsx
+++ src/components/main-page/MainPage.tsx
@@ -4,13 +4,14 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { PMemViewer } from '../pmem-viewer/PMemViewer.tsx';
 import { RegViewer } from '../reg-viewer/RegViewer.tsx';
-import styles from '../App.module.css';
 import { EmulatorContext } from '../App.tsx';
 import { FlagsViewer } from '../flags-viewer/FlagsViewer.tsx';
 import { HexViewer } from '../hex/HexViewer.tsx';
 import { toHex } from '../../asm/asm-util.ts';
 import { StackViewer } from '../stack-viewer/StackViewer.tsx';
 import { RamViewer } from '../ram-viewer/RamViewer.tsx';
+import { Container } from '@mui/material';
+import styles from './MainPage.module.css';
 
 export function MainPage() {
   const { initEmulator, emulatorState, step: emulatorStep, portInput } = useContext(EmulatorContext);
@@ -33,6 +34,7 @@ export function MainPage() {
 
   function compile() {
     setError(null);
+    setPortInputValue('');
     try {
       initEmulator(asmCode);
     } catch (error) {
@@ -68,76 +70,90 @@ export function MainPage() {
 
   return (
     <>
-      <Box>
-        <TextField
-          label='ASM Code'
-          value={asmCode}
-          onChange={(textArea) => setAsmCode(textArea.target.value)}
-          multiline
-          fullWidth
-          inputProps={{ style: { fontFamily: 'RobotoMono, sans-serif' } }}
-        />
-        <Button
-          variant="contained"
-          onClick={compile}
-        >
-          Compile
-        </Button>
-        <Button
-          variant="text"
-          onClick={step}
-          disabled={emulatorState === null}
-        >
-          Step
-        </Button>
-        {
-          emulatorState &&
-          <Box>
-            <Box>Cycle (decimal) {emulatorState.cycle}</Box>
-            <Box>PC (hex) {toHex([emulatorState.PC])}</Box>
-            <PMemViewer
-              machineCode={emulatorState.PMEM}
-              highlightByte={emulatorState.PC}
-              highlightSize={emulatorState.asmLines[emulatorState.lineIndex]?.getSizeInBytes()}
+      <Container>
+        <Box sx={{ display: 'flex', justifyContent: 'space-around' }}>
+          <Box sx={{ width: 500 }}>
+            <TextField
+              label='ASM Code'
+              value={asmCode}
+              onChange={(textArea) => setAsmCode(textArea.target.value)}
+              multiline
+              fullWidth
+              inputProps={{ style: { fontFamily: 'RobotoMono, sans-serif' } }}
             />
-            <RegViewer
-              registers={emulatorState.registers}
-            />
-            <StackViewer
-              machineCode={emulatorState.stack}
-            />
-            <FlagsViewer
-              ZF={emulatorState.ZF}
-              COUTF={emulatorState.COUTF}
-              MSBF={emulatorState.MSBF}
-              LSBF={emulatorState.LSBF}
-            />
-            <HexViewer
-              title="Input Ports"
-              binaryData={emulatorState.inputPorts}
-            />
-            <HexViewer
-              title="Output Ports"
-              binaryData={emulatorState.outputPorts}
-            />
-            <RamViewer
-              machineCode={emulatorState.RAM}
-            />
+            <Box className={styles.buttonsContainer}>
+              <Box>
+                <Button
+                  variant="contained"
+                  onClick={compile}
+                >
+                  Compile
+                </Button>
+              </Box>
+              <Box>
+                <Button
+                  variant="text"
+                  onClick={step}
+                  disabled={emulatorState === null}
+                >
+                  Step
+                </Button>
+              </Box>
+            </Box>
           </Box>
-        }
-        {
-          emulatorState?.isWaitingPortInput &&
-          <TextField
-            label="Port Input (dec, hex or bin)"
-            value={portInputValue}
-            onChange={(textArea) => setPortInputValue(textArea.target.value)}
-          />
-        }
-        {
-          error &&
-          <Box className={styles.errorContainer}>{error}</Box>
-        }
-      </Box>
+          <Box sx={{ width: 500 }}>
+            {
+              error &&
+              <Box className={styles.errorContainer}>{error}</Box>
+            }
+            {
+              emulatorState &&
+              <Box className={styles.emulatorStateContainer}>
+                {
+                  emulatorState?.isWaitingPortInput &&
+                  <Box>
+                    <TextField
+                      label="Port Input (dec, hex or bin)"
+                      value={portInputValue}
+                      onChange={(textArea) => setPortInputValue(textArea.target.value)}
+                    />
+                  </Box>
+                }
+                <Box>Cycle (decimal) {emulatorState.cycle}</Box>
+                <Box>PC (hex) {toHex([emulatorState.PC])}</Box>
+                <PMemViewer
+                  machineCode={emulatorState.PMEM}
+                  highlightByte={emulatorState.PC}
+                  highlightSize={emulatorState.asmLines[emulatorState.lineIndex]?.getSizeInBytes()}
+                />
+                <RegViewer
+                  registers={emulatorState.registers}
+                />
+                <StackViewer
+                  machineCode={emulatorState.stack}
+                />
+                <FlagsViewer
+                  ZF={emulatorState.ZF}
+                  COUTF={emulatorState.COUTF}
+                  MSBF={emulatorState.MSBF}
+                  LSBF={emulatorState.LSBF}
+                />
+                <HexViewer
+                  title="Input Ports"
+                  binaryData={emulatorState.inputPorts}
+                />
+                <HexViewer
+                  title="Output Ports"
+                  binaryData={emulatorState.outputPorts}
+                />
+                <RamViewer
+                  machineCode={emulatorState.RAM}
+                />
+              </Box>
+            }
+          </Box>
+        </Box>
+      </Container>
     </>
   );
 }
